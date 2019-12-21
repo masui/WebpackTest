1a2499a0e0b13b34edee306746d58b1a582f3fdd
Toshiyuki Masui <masui@pitecan.com>
2019-12-22 03:40:35 +0900

    Initial

---
+++ main.js
@@ -0,0 +1,13 @@
+//import Vue from 'vue'
+//var Vue = require('vue');
+
+import Vue from "vue/dist/vue.esm.js";
+
+var demo = new Vue({
+    el: '#demo',
+    data: {
+	message: 'Hello Vue.js!'
+    }
+});
+
+//})
